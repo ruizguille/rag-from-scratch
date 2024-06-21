50779aced3141dee5aec3c68c1e99275cdfbafb9
ruizguille <ruiz.ruiz.guillermo@gmail.com>
2024-06-21 19:23:10 +0200

    Complete vector store query with cosine similarity

--- app/vector_store.py
+++ app/vector_store.py
@@ -3,6 +3,12 @@ import numpy as np
 
 VECTOR_STORE_FILEPATH = 'data/vector_store.json'
 
+def cosine_similarity(query_vector, vectors):
+    """Calculates the cosine similarity between a query vector and a list of vectors."""
+    query_vector = np.array(query_vector)
+    vectors = np.array(vectors)
+    return np.dot(vectors, query_vector) / (np.linalg.norm(vectors, axis=1) * np.linalg.norm(query_vector))
+
 class VectorStore:
     def __init__(self):
         self.store = []
@@ -20,3 +26,10 @@ class VectorStore:
     def load(self, file_path=VECTOR_STORE_FILEPATH):
         with open(file_path, 'r') as f:
             self.store = json.load(f)
+    
+    def query(self, vector, top_k=5):
+        vectors = [v['vector'] for v in self.store]
+        similarities = cosine_similarity(vector, vectors)
+        print('Shape', similarities.shape)
+        top_k_indices = np.argsort(similarities)[-top_k:][::-1]
+        return [{**self.store[i], 'score': similarities[i]} for i in top_k_indices]
